1dd558c9fa06df209dd5e3ebd5c89d12c50c669a
Pongpun364 <pongpun364@hotmail.com>
2021-11-04 00:50:09 +0700

    'updateadmintosupportsuctomusermodel'

--- app/core/models.py
+++ app/core/models.py
@@ -30,7 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default = False)
 
-    objects = UserManager()
+    objects = UserManager()  # class attribute ทุกๆ instance จะมีเหมือนกันหมด เพิ่งเคยเห็นการ inherite เเบบนี้ ว้าวๆๆ
 
     USERNAME_FIELD = 'email'
 
